ed1535adf25ff5db77ec49e3f288b5e6fa54534d
EdwardMelendezM <192666@unsaac.edu.pe>
2023-08-12 15:03:44 -0500

    adding import modules

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HijoComponent } from './hijo/hijo.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HijoComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HijoComponent
   ]
 })
 export class ComponentsModule { }
