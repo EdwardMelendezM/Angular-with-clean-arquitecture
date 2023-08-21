b5b5aae38e7644daf8ff5f5a61e4a4689533123f
EdwardMelendezM <192666@unsaac.edu.pe>
2023-08-21 12:06:46 -0500

    adding local storage wit get and set

--- src/presentation/app/components/sidebar/sidebar.component.ts
+++ src/presentation/app/components/sidebar/sidebar.component.ts
@@ -14,6 +14,10 @@ export class SidebarComponent {
     return this.gifsService.tagsHistory;
   }
 
+  onClick(tag:string){
+    this.gifsService.searchTag(tag);
+  }
+
   
 
 }
--- src/presentation/app/pages/git-app/services/gifs.service.ts
+++ src/presentation/app/pages/git-app/services/gifs.service.ts
@@ -10,7 +10,10 @@ export class GifsService {
   private serviceUrl: string = "https://api.giphy.com/v1/gifs"
   private apiKey: string = "uPXnAZMiN9IxNL5GnLXIR46XQQoQyCSr"
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {
+    this.loadLocalStorage();
+    this.loadFirstGifs();
+  }
 
   get tagsHistory(){
     return [...this._tagsHistory]
@@ -19,6 +22,17 @@ export class GifsService {
   get gifList(){
     return [...this._gifList]
   }
+
+  private saveLocalStorage():void{
+    localStorage.setItem("history", JSON.stringify(this._tagsHistory) )
+  }
+
+  private loadLocalStorage():void{
+    const temp = localStorage.getItem("history")
+    if(!temp) return 
+    this._tagsHistory = JSON.parse(temp)
+  }
+
 
   private organizeHistory(tag: string){
     tag = tag.toLowerCase()
@@ -29,6 +43,12 @@ export class GifsService {
 
     this._tagsHistory.unshift(tag)
     this._tagsHistory = this.tagsHistory.splice(0,10)
+    this.saveLocalStorage()
+  }
+
+  private loadFirstGifs():void{
+    if (this._tagsHistory.length===0) return
+    this.searchTag(this._tagsHistory[0])
   }
 
   searchTag( tag:string ) :void {
