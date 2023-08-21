12fdb622e7af45089e0693d6f4b6f5cc95dfedc5
EdwardMelendezM <192666@unsaac.edu.pe>
2023-08-20 19:14:16 -0500

    using httpClient

--- src/presentation/app/pages/git-app/services/gifs.service.ts
+++ src/presentation/app/pages/git-app/services/gifs.service.ts
@@ -1,12 +1,14 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
   private _tagsHistory: string[]=[]
+  private serviceUrl: string = "https://api.giphy.com/v1/gifs"
+  private apiKey: string = "uPXnAZMiN9IxNL5GnLXIR46XQQoQyCSr"
 
-
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   get tagsHistory(){
     return [...this._tagsHistory]
@@ -23,9 +25,20 @@ export class GifsService {
     this._tagsHistory = this.tagsHistory.splice(0,10)
   }
 
-  searchTag( tag:string ){
+  searchTag( tag:string ) :void {
     if(tag.trim().length<2) return 
     this.organizeHistory(tag)
+
+
+    const params = new HttpParams()
+      .set('api_key',this.apiKey)
+      .set('limit','10')
+      .set('q',tag)
+
+    this.http.get(`${this.serviceUrl}/search`,{ params })
+    .subscribe(resp=>{
+      console.log(resp)
+    })
   }
   
 }
